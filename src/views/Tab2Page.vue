d253c974119cb4a110a4ce5ff41acf942a74e156
TykeaFZ14 <tykea.20033@mhs.unesa.ac.id>
2023-03-09 16:00:39 +0700

    Update History

--- src/views/Tab2Page.vue
+++ src/views/Tab2Page.vue
@@ -8,11 +8,115 @@
     <ion-content :fullscreen="true">
       <ion-header collapse="condense">
         <ion-toolbar>
-          <ion-title size="large">Tab 2</ion-title>
+          <ion-title size="large">History</ion-title>
         </ion-toolbar>
       </ion-header>
 
-      <ExploreContainer name="Tab 2 page" />
+<!-- list history -->
+<ion-card>
+    <ion-card-content>
+      <ion-toolbar>
+      <ion-label>
+        <h2>Cannacht Open House</h2>
+      </ion-label>
+      <ion-label slot="end">
+          <h3>Visited at 09/03/23 13:47</h3>
+      </ion-label>
+    </ion-toolbar>
+    </ion-card-content>
+</ion-card>
+
+<ion-card>
+    <ion-card-content>
+      <ion-toolbar>
+      <ion-label>
+        <h2>Lettucepry Closed House</h2>
+      </ion-label>
+      <ion-label slot="end">
+          <h3>Visited at 05/03/23 12:00</h3>
+      </ion-label>
+    </ion-toolbar>
+    </ion-card-content>
+</ion-card>
+
+<ion-card>
+    <ion-card-content>
+      <ion-toolbar>
+      <ion-label>
+        <h2>Rachtine Closed House</h2>
+      </ion-label>
+      <ion-label slot="end">
+          <h3>Visited at 23/02/23 09:09</h3>
+      </ion-label>
+    </ion-toolbar>
+    </ion-card-content>
+</ion-card>
+
+<ion-card>
+    <ion-card-content>
+      <ion-toolbar>
+      <ion-label>
+        <h2>Silverclocks Open House</h2>
+      </ion-label>
+      <ion-label slot="end">
+          <h3>Visited at 15/02/23 08:54</h3>
+      </ion-label>
+    </ion-toolbar>
+    </ion-card-content>
+</ion-card>
+
+<ion-card>
+    <ion-card-content>
+      <ion-toolbar>
+      <ion-label>
+        <h2>Seraphine Closed House</h2>
+      </ion-label>
+      <ion-label slot="end">
+          <h3>Visited at 02/02/23 02:02</h3>
+      </ion-label>
+    </ion-toolbar>
+    </ion-card-content>
+</ion-card>
+
+<ion-card>
+    <ion-card-content>
+      <ion-toolbar>
+      <ion-label>
+        <h2>Slyking Closed House</h2>
+      </ion-label>
+      <ion-label slot="end">
+          <h3>Visited at 27/01/23 14:03</h3>
+      </ion-label>
+    </ion-toolbar>
+    </ion-card-content>
+</ion-card>
+
+<ion-card>
+    <ion-card-content>
+      <ion-toolbar>
+      <ion-label>
+        <h2>Laceton Open House</h2>
+      </ion-label>
+      <ion-label slot="end">
+          <h3>Visited at 23/01/23 07:52</h3>
+      </ion-label>
+    </ion-toolbar>
+    </ion-card-content>
+</ion-card>
+
+<ion-card>
+    <ion-card-content>
+      <ion-toolbar>
+      <ion-label>
+        <h2>Lessangry Closed House</h2>
+      </ion-label>
+      <ion-label slot="end">
+          <h3>Visited at 17/01/23 09:44</h3>
+      </ion-label>
+    </ion-toolbar>
+    </ion-card-content>
+</ion-card>
+
     </ion-content>
   </ion-page>
 </template>
--- src/views/TabsPage.vue
+++ src/views/TabsPage.vue
@@ -4,7 +4,7 @@
       <ion-router-outlet></ion-router-outlet>
       <ion-tab-bar slot="bottom">
         <ion-tab-button tab="tab1" href="/tabs/tab1">
-          <ion-icon aria-hidden="true" :icon="triangle" />
+          <ion-icon aria-hidden="true" :icon="ellipse" />
           <ion-label>Category</ion-label>
         </ion-tab-button>
 
@@ -14,7 +14,7 @@
         </ion-tab-button>
 
         <ion-tab-button tab="tab3" href="/tabs/tab3">
-          <ion-icon aria-hidden="true" :icon="square" />
+          <ion-icon aria-hidden="true" :icon="ellipse" />
           <ion-label>Setting</ion-label>
         </ion-tab-button>
       </ion-tab-bar>

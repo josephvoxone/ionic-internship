0ef2cd56405922f4baee748deccf4150e44d336b
TykeaFZ14 <126534254+TykeaFZ14@users.noreply.github.com>
2023-03-20 23:36:47 +0700

    Edit Interface

--- src/views/Tab2Page.vue
+++ src/views/Tab2Page.vue
@@ -1,7 +1,7 @@
 <template>
   <ion-page>
     <ion-header>
-      <ion-toolbar>
+      <ion-toolbar color="success">
         <ion-title class="ion-text-center">History</ion-title>
       </ion-toolbar>
     </ion-header>

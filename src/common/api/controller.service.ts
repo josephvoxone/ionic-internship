7c027e2906c15944ce3f2b11ef3fbfe73c2b2093
Joseph <josephshandyharvian@gmail.com>
2023-03-21 10:49:30 +0700

    Update Boilerplate

---
+++ src/common/api/controller.service.ts
@@ -0,0 +1,93 @@
+import { toastController, loadingController, alertController } from "@ionic/vue";
+import { alertCircle, close, checkmarkDone } from "ionicons/icons";
+
+const toast = {
+    // Toast
+    async presentToast(data: any) {
+        const toast = await toastController.create({
+            header: data?.header,
+            message: data?.msg,
+            position: "bottom",
+            color: data?.color,
+            duration: data?.duration || 2000,
+            mode: "ios",
+            buttons: [
+                {
+                    side: data?.side || "start",
+                    icon: data?.icon || checkmarkDone,
+                },
+                data?.handler || {
+                    side: "right",
+                    icon: close,
+                },
+            ],
+        });
+        return toast.present();
+    },
+    // Toast
+    async errorToast(data: any) {
+        const toast = await toastController.create({
+            header: data?.header,
+            message: data?.message || "Sepertinya ada kesalahan.",
+            position: "bottom",
+            color: "danger",
+            duration: data?.duration || 2000,
+            mode: "ios",
+            buttons: [
+                {
+                    side: data?.side || "start",
+                    icon: data?.icon || alertCircle,
+                },
+                data?.handler || {
+                    side: "right",
+                    icon: close,
+                },
+            ],
+        });
+        return toast.present();
+    },
+};
+const loading = {
+    //Loading
+    async open(data: any) {
+        const loading = await loadingController.create({
+            message: data?.message,
+            spinner: "dots",
+            backdropDismiss: false,
+        });
+
+        return loading.present();
+    },
+    async close() {
+        loadingController.dismiss();
+    },
+};
+const alert = {
+    async presentAlert(data: any) {
+        const alert = await alertController.create({
+            cssClass: 'alert-apps',
+            header: data?.header,
+            message: data?.message,
+            buttons: data?.buttons || [
+                {
+                    role: 'cancel',
+                    text: data?.cancel?.text || 'Batal',
+                    handler: () => data?.cancel?.handler,
+                },
+                {
+                    role: 'oke',
+                    text: data?.ok?.text || 'Iya',
+                    handler: () => data?.ok?.handler,
+                },
+            ],
+        });
+        return alert.present();
+    }
+
+}
+
+export default {
+    toast,
+    loading,
+    alert
+};
---
+++ src/common/services/kandang.service.ts
@@ -0,0 +1,26 @@
+import http from "@/common/api/axios.service";
+
+class PromoService {
+    getPromos(params: any) {
+        return http.get(`/example`, { params })
+            .then((res) => res)
+            .catch(error => { throw error })
+    }
+
+    addPromo(params: any) {
+        return http.post(`/example`, params)
+            .then(response => response.data.data)
+            .catch(error => { throw error })
+    }
+
+    updatePromo(params: any) {
+        return http.put(`/example`, { params })
+            .then((res) => res.data.data[0])
+            .catch(error => { throw error })
+    }
+
+    deletePromo(id: any) {
+        return http.delete(`/example/${id}`)
+    }
+}
+export default new PromoService();
